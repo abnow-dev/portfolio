3a8c6c8b959816bdf29c1b1d995673d43ec91ec4
Abhinav Singirthi <abhinav_singirthi@Abhinavs-MacBook-Air.local>
2024-09-01 20:31:37 +0530

    toggle css

--- src/MainSection.jsx
+++ src/MainSection.jsx
@@ -28,137 +28,170 @@ const MainSection = () => {
     });
   }, [activeTab]);
 
+  const renderCard = (CardContent, key, extraClass = '', title = '') => (
+    <Card className={extraClass} key={key} title={title}>
+      {CardContent}
+    </Card>
+  );
+  
   const renderCards = () => {
-    let cards = [];
+    const cards = [];
+    
+    const aboutCard = renderCard(
+      <div className="card-content">
+        <img src="abhinav_profile.jpg" alt="Profile Icon" />
+        <div>
+          <h4>Hi, I'm Abhinav!</h4>
+          <p>
+            Software Engineer dedicated to creating intuitive and visually appealing user interfaces.
+          </p>
+        </div>
+      </div>, 
+      'about-card', 
+      'about-card'
+    );
+  
+    const languagesCard = renderCard(
+      <ul>
+        <li>English</li>
+        <li>Hindi</li>
+        <li>Telugu</li>
+      </ul>,
+      'language-card',
+      'languages-card',
+      'Languages'
+    );
+  
+    const socialContainer = (
+      <div className="social-container" key="social-container">
+        <Card className="image-only-card linkedin-card" key="linkedin-card">
+          <a href="https://www.linkedin.com/in/abhinav-singirthi-158664148/" target="_blank" rel="noopener noreferrer">
+            <img src="linkedin_logo.svg" alt="LinkedIn" />
+          </a>
+        </Card>
+        <Card className="image-only-card github-card" key="github-card">
+          <a href="https://github.com/abnow-dev?tab=repositories" target="_blank" rel="noopener noreferrer">
+            <img src="github_logo.svg" alt="GitHub" />
+          </a>
+        </Card>
+      </div>
+    );
+  
+    const certificatesCard = renderCard(
+      <div className='cert-link-container'>
+        {[
+          { href: 'https://www.hackerrank.com/certificates/d48784dd6413', text: 'Frontend Developer (React) Certificate' },
+          { href: 'https://www.hackerrank.com/certificates/c3ae7db81b46', text: 'React (Basic) Certificate' },
+          { href: 'https://www.hackerrank.com/certificates/b63fe14db229', text: 'Angular (Intermediate) Certificate' },
+          { href: 'https://www.hackerrank.com/certificates/734aea618165', text: 'Angular (Basic) Certificate' },
+          { href: 'https://www.hackerrank.com/certificates/348454a7388e', text: 'Python (Basic) Certificate' },
+          { href: 'https://www.credly.com/badges/4bfb3812-4b98-4a43-8d80-85ec22c4fa78?source=linked_in_profile', text: 'Microsoft Certified: Azure Fundamentals' }
+        ].map((link, index) => (
+          <div key={index} className='cert-link-container'>
+            <a href={link.href} target='_blank' rel="noopener noreferrer">{link.text}</a>
+          </div>
+        ))}
+      </div>,
+      'certificates-card',
+      'certificates-card',
+      'Certificates'
+    );
+  
+    const experienceCard = renderCard(
+      <ul><br/>
+        <li>Cisco: UI Engineer (2021)</li><br/>
+        <li>Capgemini : Associate Consultant (2019 - 2021)</li>
+      </ul>,
+      'experience-card',
+      'experience-card',
+      'Experience'
+    );
+  
+    const certificatesExperienceContainer = (
+      <div className="certificates-experience-container" key="cert-exp-container">
+        {certificatesCard}
+        {experienceCard}
+      </div>
+    );
+  
+    const projectsCard = renderCard(
+      <div className='project-container'>
+        {[
+          'Infra (Zero Touch Provisioning)',
+          'Webex: Hybrid Services (Video Mesh)',
+          'Webex: Hybrid Service (Hybrid Data Security)',
+          'TORCH (Total Onestop Reporting for Corporate Healthcare)'
+        ].map((project, index) => (
+          <details key={index} className='project-detail-container'>
+            <summary>{project}</summary>
+          </details>
+        ))}
+      </div>,
+      'projects-card',
+      'projects-card'
+    );
+  
+    const skillsCard = renderCard(
+      <div className="skills-icons">
+        {[
+          { src: "angular_logo.svg", alt: "Angular Icon", title: "Angular" },
+          { src: "react_logo.svg", alt: "React.js Icon", title: "React.js" },
+          { src: "javascript_logo.svg", alt: "JavaScript Icon", title: "JavaScript" },
+          { src: "typescript_logo.svg", alt: "Typescript Icon", title: "Typescript" },
+          { src: "jquery_logo.svg", alt: "jQuery Icon", title: "jQuery" },
+          { src: "html_logo.svg", alt: "HTML Icon", title: "HTML" },
+          { src: "css_logo.svg", alt: "CSS Icon", title: "CSS" },
+          { src: "bootstrap_logo.svg", alt: "Bootstrap Icon", title: "Bootstrap" },
+          { src: "sql_logo.svg", alt: "SQL Icon", title: "SQL" },
+          { src: "nodejs_logo.svg", alt: "Node.js Icon", title: "Node.js" },
+          { src: "python_logo.svg", alt: "Python Icon", title: "Python" },
+          { src: "docker_logo.svg", alt: "Docker Icon", title: "Docker" }
+        ].map((skill, index) => (
+          <img key={index} src={skill.src} alt={skill.alt} title={skill.title} />
+        ))}
+      </div>,
+      'skills-card',
+      'skills-card'
+    );
+  
+    const themeToggleCard = renderCard(
+      <div className="toggle-button" onClick={toggleTheme}>
+        <div className={`toggle-circle ${isDarkTheme ? 'dark' : 'light'}`}></div>
+      </div>,
+      'theme-toggle-card',
+      'theme-toggle-card',
+      !isDarkTheme ? 'Dark Mode': 'light Mode'
+    );
 
+    const resumeCard = renderCard(
+      <div className='resume-container'>
+        <a href='abhinav_singirthi.pdf' target='_blank' rel='noopener noreferrer' download='Abhinav_Singirthi_Resume.pdf'>
+          <img src='download_svg.webp' alt='Download Resume' height={50} width={50} />
+        </a>
+      </div>,
+      'resume-card',
+      'resume-card',
+      'Resume'
+    );    
+    
+  
     if (activeTab === 'all') {
-      cards.push(
-        <Card className="about-card" key="about-card">
-          <div className="card-content">
-            <img src="abhinav_profile.jpg" alt="Profile Icon" />
-            <div>
-              <h4>Hi, I'm Abhinav!</h4>
-              <p>
-                Software Engineer dedicated to creating intuitive and visually appealing user interfaces.
-              </p>
-            </div>
-          </div>
-        </Card>
-      );
-      cards.push(
-        <Card title="Skills" className="skills-card" key="skills-card">
-          <div className="skills-icons">
-            <img src="angular_logo.svg" alt="Angular Icon" title="Angular" />
-            <img src="react_logo.svg" alt="React.js Icon" title="React.js" />
-            <img src="javascript_logo.svg" alt="JavaScript Icon" title="JavaScript" />
-            <img src="typescript_logo.svg" alt="Typescript Icon" title="Typescript" />
-            <img src="jquery_logo.svg" alt="jQuery Icon" title="jQuery" />
-            <img src="html_logo.svg" alt="HTML Icon" title="HTML" />
-            <img src="css_logo.svg" alt="CSS Icon" title="CSS" />
-            <img src="bootstrap_logo.svg" alt="Bootstrap Icon" title="Bootstrap" />
-            <img src="sql_logo.svg" alt="SQL Icon" title="SQL" />
-            <img src="nodejs_logo.svg" alt="Node.js Icon" title="Node.js" />
-            <img src="python_logo.svg" alt="Python Icon" title="Python" />
-            <img src="docker_logo.svg" alt="Docker Icon" title="Docker" />
-          </div>
-        </Card>
-      );
-
-      cards.push(
-        <div className="certificates-experience-container" key="cert-exp-container">
-          <Card title="Certificates" className="certificates-card" key="certificates-card">
-            <div className='cert-link-container'>
-            <div className='cert-link-container'>
-              <a href='https://www.hackerrank.com/certificates/d48784dd6413' target='_blank' rel="noopener noreferrer">
-                Frontend Developer (React) Certificate
-              </a>
-            </div>
-            <div className='cert-link-container'>
-              <a href='https://www.hackerrank.com/certificates/c3ae7db81b46' target='_blank' rel="noopener noreferrer">
-                React (Basic) Certificate
-              </a>
-            </div>
-            <div className='cert-link-container'>
-              <a href='https://www.hackerrank.com/certificates/b63fe14db229' target='_blank' rel="noopener noreferrer">
-                Angular (Intermediate) Certificate
-              </a>
-            </div>
-            <div className='cert-link-container'>
-              <a href='https://www.hackerrank.com/certificates/734aea618165' target='_blank' rel="noopener noreferrer">
-                Angular (Basic) Certificate
-              </a>
-            </div>
-            <div className='cert-link-container'>
-              <a href='https://www.hackerrank.com/certificates/348454a7388e' target='_blank' rel="noopener noreferrer">
-                Python (Basic) Certificate
-              </a>
-            </div>
-            <div className='cert-link-container'>
-              <a href='https://www.credly.com/badges/4bfb3812-4b98-4a43-8d80-85ec22c4fa78?source=linked_in_profile' target='_blank' rel="noopener noreferrer">
-              Microsoft Certified: Azure Fundamentals
-              </a>
-            </div>
-            </div>
-          </Card>
-          <Card title="Total Experience: 5.3+ years" className="experience-card" key="experience-card">
-            <ul><br/>
-              <li>Cisco: UI Engineer (2021)</li><br/>
-              <li>Capgemini : Associate Consultant (2019 - 2021)</li>
-            </ul>
-          </Card>
-        </div>
-      );
-
-      cards.push(
-        <div className="social-container" key="social-container">
-          <Card className="image-only-card linkedin-card" key="linkedin-card">
-            <img src='linkedin_logo.svg' alt="LinkedIn" />
-          </Card>
-          <Card className="image-only-card github-card" key="github-card">
-            <img src='github_logo.svg' alt="GitHub" />
-          </Card>
-        </div>
-      );
-
-      cards.push(
-        <Card title="Projects" className="projects-card" key="projects-card">
-          <div className='project-container'>
-            <details className='project-detail-container' open>
-              <summary>Infra (Zero Touch Provisioning)</summary>
-            </details>
-            <details className='project-detail-container'>
-              <summary>Webex: Hybrid Services (Video Mesh)</summary>
-            </details>
-            <details className='project-detail-container'>
-              <summary>Webex: Hybrid Service (Hybrid Data Security)</summary>
-            </details>
-            <details className='project-detail-container'>
-              <summary>TORCH (Total Onestop Reporting for Corporate Healthcare)</summary>
-            </details>
-          </div>
-        </Card>
-      );
-      cards.push(
-        <Card title="Languages" className="languages-card" key="language-card">
-          <ul>
-            <li>English</li>
-            <li>Hindi</li>
-            <li>Telugu</li>
-          </ul>
-        </Card>
-      );
-      cards.push(
-        <Card title={!isDarkTheme ? 'Dark Mode' : 'Light Mode'} className="theme-toggle-card" key="theme-toggle-card">
-          <div className="toggle-button" onClick={toggleTheme}>
-            <div className={`toggle-circle ${isDarkTheme ? 'dark' : 'light'}`}></div>
-          </div>
-        </Card>
-      );
+      cards.push(aboutCard, skillsCard, certificatesExperienceContainer, socialContainer, projectsCard, languagesCard, resumeCard);
     }
-
+  
+    if (activeTab === 'about') {
+      cards.push(aboutCard, languagesCard);
+    }
+  
+    if (activeTab === 'work') {
+      cards.push(projectsCard, socialContainer, certificatesExperienceContainer, skillsCard, resumeCard);
+    }
+  
+    cards.push(themeToggleCard);
+  
     return cards;
   };
-
+  
   return (
     <div>
       <Tabs onChange={handleTabChange} />
