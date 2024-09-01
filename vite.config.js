143a7345cb9b184199423bed206403fd5ff65843
Abhinav Singirthi <abhinav_singirthi@Abhinavs-MacBook-Air.local>
2024-09-01 14:09:07 +0530

    Update build settings

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // For root
+  base: '/', // Ensure this is set correctly for GitHub Pages
 });
